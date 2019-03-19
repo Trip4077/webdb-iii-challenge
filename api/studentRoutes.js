f5b9450a82409e9a010144efbba37763fd44ca13
Trip4077 <bj2336@email.vccs.edu>
2019-03-19 12:34:14 -0400

    GET student by ID and POST student implemented

--- api/studentRoutes.js
+++ api/studentRoutes.js
@@ -6,6 +6,21 @@ const knexConfig = require('../knexfile');
 
 const db = knex(knexConfig.development);
 
+//POST STUDENT
+router.post('/', (req, res) => {
+    const student = req.body;
+
+    db.insert(student)
+      .into('students')
+      .then(student => {
+          res.status(201).json(student[0])
+      })
+      .catch(err => {
+          console.log(err);
+      })
+})
+
+//GET ALL STUDENTS
 router.get('/', (req, res) => {
     db('students')
       .then(students => {
@@ -15,5 +30,21 @@ router.get('/', (req, res) => {
           console.log(err);
       })
 })
+
+//GET STUDENT by ID
+router.get('/:id', (req, res) => {
+    const { id }  = req.params;
+    
+    db('students')
+      .where({ id })
+      .then(student => {
+          res.json(student[0]);
+      })
+      .catch(err => {
+          console.log(err);
+      })
+})
+
+
 
 module.exports = router;
