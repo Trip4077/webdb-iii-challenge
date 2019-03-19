f34fa0b994acb8cff8e49adda96ccedadd601ef0
Trip4077 <bj2336@email.vccs.edu>
2019-03-19 12:04:57 -0400

    Get Cohort By ID and Add New Cohort Implemented

--- api/cohortRoutes.js
+++ api/cohortRoutes.js
@@ -6,6 +6,21 @@ const knexConfig = require('../knexfile');
 
 const db = knex(knexConfig.development);
 
+//POST COHORT
+router.post('/', (req, res) => {
+    const cohort = req.body
+    console.log(cohort)
+    db.insert(cohort)
+      .into('cohorts')
+      .then(id => {
+          res.status(201).json(id[0])
+      })
+      .catch(err => {
+          console.log(err);
+      })
+})
+
+//GET ALL COHORTS
 router.get('/', (req, res) => {
     db('cohorts')
       .then(cohorts => {
@@ -14,6 +29,18 @@ router.get('/', (req, res) => {
       .catch(err => {
           console.log(err);
       })
+})
+
+//GET COHORT by ID
+router.get('/:id', (req, res) => {
+   db('cohorts')
+     .where({ id: req.params.id })
+     .then(cohort => {
+            res.json(cohort[0]);
+     })
+     .catch(err => {
+         console.log(err);
+     }) 
 })
 
 
